9f5e4b7f168a1ec4d9b5f8ac11447c50f3a3d0c0
Ashik150 <sskhan4409@gmail.com>
2025-08-20 01:17:49 +0600

    necessary file upload

---
+++ Backend/mailtrap/email.js
@@ -0,0 +1,2 @@
+import { PASSWORD_RESET_REQUEST_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE,PASSWORD_RESET_SUCCESS_TEMPLATE } from "./emailTemplates.js";
+import { mailtrapClient,sender } from "./mailtrap.config.js";
