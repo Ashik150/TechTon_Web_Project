2e7af178f8add80e9994c02beeb1bf05e7d200bc
Ashik150 <sskhan4409@gmail.com>
2025-08-20 01:18:27 +0600

    sendverificationemail api done

--- Backend/mailtrap/email.js
+++ Backend/mailtrap/email.js
@@ -1,2 +1,20 @@
 import { PASSWORD_RESET_REQUEST_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE,PASSWORD_RESET_SUCCESS_TEMPLATE } from "./emailTemplates.js";
 import { mailtrapClient,sender } from "./mailtrap.config.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+    const recipient = [{ email }]
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Verify your email",
+            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
+            category: "Email Verification"
+        })
+        console.log("Email sent successfully", response);
+    } catch (error) {
+        console.error("Error sending email:", error.message);
+        throw new Error(`Error sending email:${error}`);
+    }
+}
