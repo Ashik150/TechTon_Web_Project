3960e9535fe3bfee9552228b430fa1962be66f49
Ashik150 <sskhan4409@gmail.com>
2025-08-20 21:43:20 +0600

    cors setup

--- Backend/index.js
+++ Backend/index.js
@@ -18,3 +18,8 @@ import cloudinary from 'cloudinary';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
