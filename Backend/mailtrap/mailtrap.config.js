9cb1ee58781062696f7b20fb109b64f14c951c60
Ashik150 <sskhan4409@gmail.com>
2025-08-20 08:51:23 +0600

    mailtrap sender info declare

--- Backend/mailtrap/mailtrap.config.js
+++ Backend/mailtrap/mailtrap.config.js
@@ -6,3 +6,7 @@ dotenv.config();
 export const mailtrapClient = new MailtrapClient({
   token: process.env.MAILTRAP_TOKEN,
 });
+export const sender = {
+  email: "mailtrap@demomailtrap.com",
+  name: "TechTon Team",
+};
