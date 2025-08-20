e8a493eb4b69186bda2c77ea32ddacce40ac43ff
Ayonspoiler <ayon4052@gmail.com>
2025-08-20 15:54:41 +0600

    User Schema Created.

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,5 +1,72 @@
 import mongoose from "mongoose";
+import { timeStamp } from "node:console";
 
-const userSchema=new mongoose.Schema({
-    
-})
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastLogin: {
+    type: Date,
+    default: Date.now,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  phoneNumber: {
+    type: Number,
+    //default: null,
+  },
+  addresses: [
+    {
+      country: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      address1: {
+        type: String,
+      },
+      address2: {
+        type: String,
+      },
+      zipCode: {
+        type: Number,
+      },
+      addressType: {
+        type: String,
+      },
+    },
+  ],
+  role: {
+    type: String,
+    default: "user",
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      //required: true,
+    },
+    url: {
+      type: String,
+      // required: true,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  resetPasswordToken: String,
+  resetPasswordExpiresAt: Date,
+  verificationToken: String,
+  verificationTokenExpiresAt: Date,
+},{timestamps:true});
+
+export const User = mongoose.model("User", userSchema);
