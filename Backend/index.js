55038676bfddbdf52cf589b63d95780c70a74cab
Ashik150 <sskhan4409@gmail.com>
2025-08-20 21:51:40 +0600

    body parser and coookie parser declare

--- Backend/index.js
+++ Backend/index.js
@@ -29,3 +29,8 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
   });
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/",express.static("uploads"));
