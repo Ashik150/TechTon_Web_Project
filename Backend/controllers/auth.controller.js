96948d5c4c6370e955e4057d419803aa001d8aca
Ashik150 <sskhan4409@gmail.com>
2025-08-21 18:44:00 +0600

    login api done

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -77,3 +77,31 @@ export const verifyEmail = async (req, res) => {
         res.status(400).json({ success: false, message: error.message });
     }
 }
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
+        }
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
+        }
+        if (!user.isVerified) {
+            return res.status(400).json({ success: false, message: "You have to verify your account!" });
+        }
+
+        res.status(200).json({
+            success: true, message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
