cef82297468849e0d90e46b5b1f46b8dfd29254e
Ashik150 <sskhan4409@gmail.com>
2025-08-21 18:41:35 +0600

    signup api declaration

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -5,3 +5,32 @@ import { generateTokenAndSetCookie } from '../Utils/generateTokenAndSetCookie.js
 import { sendVerificationEmail, sendWelcomeEmail, sendPasswordResetEmail, sendResetSuccessEmail } from '../mailtrap/emails.js';
 import ErrorHandler from '../Utils/ErrorHandler.js';
 import cloudinary from 'cloudinary';
+
+export const signup = async (req, res) => {
+    const { email, password, name } = req.body;
+    try {
+        if (!email || !password || !name) {
+            throw new Error("All fields are required");
+        }
+
+        const userAlreadyExists = await User.findOne({ email });
+        if (userAlreadyExists) {
+            return res.status(400).json({ success: false, message: "User already exists" });
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 10);
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
+        });
+
+        await user.save();
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
