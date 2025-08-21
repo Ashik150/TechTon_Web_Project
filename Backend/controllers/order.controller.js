6423c9b08c2ad1378bf64dca7021bf319065332c
Ayonspoiler <ayon4052@gmail.com>
2025-08-21 11:53:01 +0600

    accept order and getOrder for admin implemented

--- Backend/controllers/order.controller.js
+++ Backend/controllers/order.controller.js
@@ -150,3 +150,58 @@ export const requestOrderRefund = async (req, res, next) => {
     return next(new ErrorHandler(error.message, 500));
   }
 };
+
+export const acceptOrderRefund = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+      return next(new ErrorHandler("Order not found with this id", 400));
+    }
+
+    order.status = req.body.status;
+
+    await order.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Order Refund successfull!",
+    });
+
+    if (req.body.status === "Refund Success") {
+      const refundPoints = Math.floor(order.totalPrice * 0.01);
+      await User.findByIdAndUpdate(order.user._id, {
+        $inc: { points: -refundPoints },
+      });
+      order.cart.forEach(async (o) => {
+        await updateOrder(o._id, o.qty);
+      });
+    }
+
+    async function updateOrder(id, qty) {
+      const product = await Product.findById(id);
+
+      product.stock += qty;
+      product.sold_out -= qty;
+
+      await product.save({ validateBeforeSave: false });
+    }
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
+
+export const getAllOrdersForAdmin = async (req, res, next) => {
+  try {
+    const orders = await Order.find().sort({
+      deliveredAt: -1,
+      createdAt: -1,
+    });
+    res.status(201).json({
+      success: true,
+      orders,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
