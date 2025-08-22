051366a8300a0326bd12f57c21d2d7d9654854e4
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-08-23 01:10:01 +0600

    initiating serveres

--- socket/index.js
+++ socket/index.js
@@ -1,5 +1,15 @@
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import express from 'express';
+import cors from 'cors';
 
-const httpServer = createServer();
-const io = new Server(httpServer);
+const app = express();
+const httpServer = createServer(app);
+// const io = new Server(httpServer);
+//initiatint server
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:4000",
+        methods: ["GET", "POST"]
+    }
+});
