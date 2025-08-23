4c26f03f687deb6d5c4a05acec66e0fc70637bf3
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-08-24 00:48:57 +0600

    Add basic JSX structure and icons

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { RxCross1 } from "react-icons/rx";
+import { IoBagHandleOutline } from "react-icons/io5";
+import styles from "../../styles/styles";
 
 const Cart = ({ setOpenCart }) => {
   return (
     <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
       <div className="fixed top-0 right-0 h-full w-[80%] 800px:w-[25%] bg-white flex flex-col overflow-y-scroll justify-between shadow-sm">
-        <h1>Cart is empty!</h1>
+        <div className="w-full h-screen flex items-center justify-center">
+          <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
+            <RxCross1
+              size={25}
+              className="cursor-pointer"
+            />
+          </div>
+          <h5>Cart is empty!</h5>
+        </div>
       </div>
     </div>
   );
