6c239e604174b804cdf266f6701ebcae1bd3401b
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 23:42:18 +0600

    Added loginShop Module.

--- Backend/controllers/shop.controller.js
+++ Backend/controllers/shop.controller.js
@@ -89,3 +89,31 @@ export const activateShop = async (req, res, next) => {
   }
 };
 
+export const loginShop = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new ErrorHandler("Please provide the all fields!", 400));
+    }
+
+    const user = await Shop.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorHandler("User doesn't exists!", 400));
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return next(
+        new ErrorHandler("Please provide the correct information", 400)
+      );
+    }
+
+    sendShopToken(user, 201, res);
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
+
