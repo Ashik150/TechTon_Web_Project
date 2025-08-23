db9e27c36983d4cbe7c56a73564935ab1c789b37
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 14:42:15 +0600

    implemeted routes for getBestSellingProducts.

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -15,3 +15,4 @@ router.get("/get-all-products-shop/:id", getProducts);
 router.delete("/delete-shop-product/:id", deleteProduct);
 router.get("/get-all-products", getAllProduct);
 router.put("/create-new-review", verifyToken, createNewReview);
+router.get("/get-best-selling-products", getBestSellingProducts);
