37dc9bf1f825e62a0d0dc1ac4e05daf82a71d5ef
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 14:09:30 +0600

    CreateProduct module Created in Product Controller.

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -4,3 +4,46 @@ import { Shop } from "../models/shop.model.js";
 import { Order } from "../models/order.model.js";
 import ErrorHandler from "../Utils/ErrorHandler.js";
 
+export const createProduct = async (req, res, next) => {
+  try {
+    const shopId = req.body.shopId;
+    const shop = await Shop.findById(shopId);
+    if (!shop) {
+      return next(new ErrorHandler("Shop not found", 400));
+    } else {
+      let images = [];
+
+      if (typeof req.body.images === "string") {
+        images.push(req.body.images);
+      } else {
+        images = req.body.images;
+      }
+
+      const imagesLinks = [];
+
+      for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.uploader.upload(images[i], {
+          folder: "products",
+        });
+
+        imagesLinks.push({
+          public_id: result.public_id,
+          url: result.secure_url,
+        });
+      }
+
+      const productData = req.body;
+      productData.images = imagesLinks;
+      productData.shop = shop;
+
+      const product = await Product.create(productData);
+
+      res.status(201).json({
+        success: true,
+        product,
+      });
+    }
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+};
