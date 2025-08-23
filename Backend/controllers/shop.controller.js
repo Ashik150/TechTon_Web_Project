5df388243a268ccab748a0d7a9d4ac0a231f1ac5
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 23:36:59 +0600

    Added activate Shop Module.

--- Backend/controllers/shop.controller.js
+++ Backend/controllers/shop.controller.js
@@ -50,3 +50,42 @@ const createActivationToken = (seller) => {
     expiresIn: "5m",
   });
 };
+
+export const activateShop = async (req, res, next) => {
+  try {
+    const { activation_token } = req.body;
+
+    const newSeller = jwt.verify(
+      activation_token,
+      process.env.ACTIVATION_SECRET
+    );
+
+    if (!newSeller) {
+      return next(new ErrorHandler("Invalid token", 400));
+    }
+    const { name, email, password, avatar, zipCode, address, phoneNumber } =
+      newSeller;
+
+    let seller = await Shop.findOne({ email });
+
+    if (seller) {
+      return next(new ErrorHandler("User already exists", 400));
+    }
+    //console.log(name, email, password, avatar, zipCode, address, phoneNumber);
+
+    seller = await Shop.create({
+      name,
+      email,
+      avatar,
+      password,
+      zipCode,
+      address,
+      phoneNumber,
+    });
+
+    sendShopToken(seller, 201, res);
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
+
