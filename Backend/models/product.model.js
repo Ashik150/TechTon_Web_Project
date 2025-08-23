df25667306d34e19e8d09eb801487f7757fcea7f
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 14:00:46 +0600

    Added required variables for product schema

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -1,2 +1,42 @@
 import mongoose from "mongoose";
 
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your product name!"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter your product description!"],
+  },
+  category: {
+    type: String,
+    required: [true, "Please enter your product category!"],
+  },
+  tags: {
+    type: String,
+  },
+  originalPrice: {
+    type: Number,
+  },
+  discountPrice: {
+    type: Number,
+    required: [true, "Please enter your product price!"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter your product stock!"],
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        //required: true,
+      },
+      url: {
+        type: String,
+        //required: true,
+      },
+    },
+  ],
+});
