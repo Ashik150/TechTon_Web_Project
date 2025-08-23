7675d81e70cdf1ad9ec3447ee41b86be8a808abd
Ayonspoiler <ayon4052@gmail.com>
2025-08-24 00:05:18 +0600

    Shop route initialized

---
+++ Backend/routes/shop.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  createShop,
+  activateShop,
+  loginShop,
+  getSeller,
+  logoutShop,
+  getShopInfo,
+  updateSeller,
+  updateShopAvatar,
+} from "../controllers/shop.controller.js";
+import { isSeller } from "../middleware/auth.middleware.js";
+const router = express.Router();
