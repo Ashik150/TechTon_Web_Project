e872cd87d8decf18603ef95f9d7bb56ec94e1f70
Ashik150 <sskhan4409@gmail.com>
2025-08-23 09:44:38 +0600

    create Conversation api done

--- Backend/controllers/conversation.controller.js
+++ Backend/controllers/conversation.controller.js
@@ -1,2 +1,29 @@
 import { Conversation } from "../models/conversation.model.js";
 import ErrorHandler from '../Utils/ErrorHandler.js';
+
+export const createConversation = async (req, res, next) => {
+    try {
+        const { groupTitle, userId, sellerId } = req.body;
+
+        const isConversationExist = await Conversation.find({ groupTitle });
+        if (isConversationExist) {
+            const conversation = isConversationExist;
+            res.status(201).json({
+                success: true,
+                conversation,
+            });
+        } else {
+            const conversation = await Conversation.create({
+                members: [userId, sellerId],
+                groupTitle: groupTitle,
+            });
+
+            res.status(201).json({
+                success: true,
+                conversation,
+            });
+        }
+    } catch (error) {
+        return next(new ErrorHandler(error.response.message), 500);
+    }
+}
