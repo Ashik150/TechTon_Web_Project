9d79788a30c29b3517bf62cdd58d9b205b70314b
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 14:06:27 +0600

    Product Controller Initialized.

---
+++ Backend/controllers/product.controller.js
@@ -0,0 +1,6 @@
+import cloudinary from "cloudinary";
+import { Product } from "../models/product.model.js";
+import { Shop } from "../models/shop.model.js";
+import { Order } from "../models/order.model.js";
+import ErrorHandler from "../Utils/ErrorHandler.js";
+
