26da5a9704ad9e3ca7be6b7a798dcee24390390a
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 14:46:13 +0600

    Updated createOrder route of Order route

--- Backend/routes/order.route.js
+++ Backend/routes/order.route.js
@@ -4,11 +4,3 @@ const router = express.Router();
 import { isSeller} from '../middleware/auth.middleware.js';
 
 router.post("/create-order", createOrder);
-router.get("/get-all-orders/:userId", getAllOrdersOfUser);
-router.get("/get-seller-all-orders/:shopId", getAllOrdersOfSeller);
-router.put("/update-order-status/:id", isSeller, updateOrderStatus);
-router.put("/order-refund/:id", requestOrderRefund);
-router.put("/order-refund-success/:id", isSeller, acceptOrderRefund);
-router.get("/delivered", getDeliveredOrders);
-router.get("/get-category-distribution/:email", getProductCategoryDistribution);
-router.get("/user-points/:userId", getUserPoints);
