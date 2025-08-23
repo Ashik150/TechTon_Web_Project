2e35bca01c6e3e1e7c5f6b5cad61955a5c4bb6c0
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 22:53:39 +0600

    Added createShop module in Shop Controller.

--- Backend/controllers/shop.controller.js
+++ Backend/controllers/shop.controller.js
@@ -4,3 +4,43 @@ import { sendShopToken } from "../Utils/shopToken.js";
 import jwt from "jsonwebtoken";
 import { ErrorHandler } from "../Utils/ErrorHandler.js";
 import cloudinary from "cloudinary";
+
+export const createShop = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const sellerEmail = await Shop.findOne({ email });
+    if (sellerEmail) {
+      return next(new ErrorHandler("User already exists", 400));
+    }
+    const seller = {
+      name: req.body.name,
+      email: email,
+      password: req.body.password,
+      avatar: {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      },
+      address: req.body.address,
+      phoneNumber: req.body.phoneNumber,
+      zipCode: req.body.zipCode,
+    };
+    const activationToken = createActivationToken(seller);
+
+    const activationUrl = `http://localhost:5173/seller/activation/${activationToken}`;
+    try {
+      await sendMail({
+        email: seller.email,
+        subject: "Activate your Shop",
+        message: `Hello ${seller.name}, please click on the link to activate your shop: ${activationUrl}`,
+      });
+      res.status(201).json({
+        success: true,
+        message: `please check your email:- ${seller.email} to activate your shop!`,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
