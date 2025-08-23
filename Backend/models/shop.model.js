a636920d8bb29f3bb0b3d969b8f0faa19cf3ff66
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 22:09:55 +0600

    Added JWT Token module in Shop Schema

--- Backend/models/shop.model.js
+++ Backend/models/shop.model.js
@@ -62,3 +62,10 @@ shopSchema.pre("save", async function (next) {
   }
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+// jwt token
+shopSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
