59a95f83d184f32e9ad03fffe6ba7b0777754fd9
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 14:56:12 +0600

    getUserPoints Route Updated.

--- Backend/routes/order.route.js
+++ Backend/routes/order.route.js
@@ -11,3 +11,4 @@ router.put("/order-refund/:id", requestOrderRefund);
 router.put("/order-refund-success/:id", isSeller, acceptOrderRefund);
 router.get("/get-delivered", getDeliveredOrders); 
 router.get("/get-category-distribution/:email", getProductCategoryDistribution);
+router.get("/user-point/:userId", getUserPoints);
