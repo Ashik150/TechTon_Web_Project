8a27d068a48a545f98d68ffeb3a70805c758eb28
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 14:54:37 +0600

    getDeliveredOrders Route Updated.

--- Backend/routes/order.route.js
+++ Backend/routes/order.route.js
@@ -9,3 +9,4 @@ router.get("/get-seller-all-orders/:shopId", getAllOrdersOfSeller);
 router.put("/update-order-status/:id", isSeller, updateOrderStatus);
 router.put("/order-refund/:id", requestOrderRefund);
 router.put("/order-refund-success/:id", isSeller, acceptOrderRefund);
+router.get("/get-delivered", getDeliveredOrders); 
