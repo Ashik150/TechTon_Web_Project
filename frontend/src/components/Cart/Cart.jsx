5fd0e4b915ddacf1fd22aa2be31388afda30ea5d
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-08-24 00:50:40 +0600

    Integrate Redux to fetch cart data

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -2,21 +2,52 @@ import React from "react";
 import { RxCross1 } from "react-icons/rx";
 import { IoBagHandleOutline } from "react-icons/io5";
 import styles from "../../styles/styles";
+import { useDispatch, useSelector } from "react-redux";
 
 const Cart = ({ setOpenCart }) => {
+  const { cart } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
   return (
     <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
       <div className="fixed top-0 right-0 h-full w-[80%] 800px:w-[25%] bg-white flex flex-col overflow-y-scroll justify-between shadow-sm">
-        <div className="w-full h-screen flex items-center justify-center">
-          <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
-            <RxCross1
-              size={25}
-              className="cursor-pointer"
-              onClick={() => setOpenCart(false)}
-            />
+        {cart && cart.length === 0 ? (
+          <div className="w-full h-screen flex items-center justify-center">
+            <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
+              <RxCross1
+                size={25}
+                className="cursor-pointer"
+                onClick={() => setOpenCart(false)}
+              />
+            </div>
+            <h5>Cart Items is empty!</h5>
           </div>
-          <h5>Cart is empty!</h5>
-        </div>
+        ) : (
+          <>
+            <div>
+              <div className="flex w-full justify-end pt-5 pr-5">
+                <RxCross1
+                  size={25}
+                  className="cursor-pointer"
+                  onClick={() => setOpenCart(false)}
+                />
+              </div>
+              {/* Item length */}
+              <div className={`${styles.noramlFlex} p-4`}>
+                <IoBagHandleOutline size={25} />
+                <h5 className="pl-2 text-[20px] font-[500]">
+                  {cart && cart.length} items
+                </h5>
+              </div>
+
+              {/* cart Single Items */}
+              <br />
+              <div className="w-full border-t">
+                {/* This will be mapped later */}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
