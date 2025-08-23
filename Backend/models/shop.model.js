ad23d731c2e69eac968bbe50946f72e58eab0485
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 22:04:17 +0600

    Added required name,email,password,description etc for Shop Schema

--- Backend/models/shop.model.js
+++ Backend/models/shop.model.js
@@ -2,4 +2,31 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-
+const shopSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your shop name!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your shop email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [6, "Password should be greater than 6 characters"],
+    select: false,
+  },
+  description: {
+    type: String,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  
+});
