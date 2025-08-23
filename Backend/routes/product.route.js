4e8161b7e03d7149bb49b247bdaf0dda592db1a8
Ayonspoiler <ayon4052@gmail.com>
2025-08-23 14:42:50 +0600

    Implemented Product Route.

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -16,3 +16,5 @@ router.delete("/delete-shop-product/:id", deleteProduct);
 router.get("/get-all-products", getAllProduct);
 router.put("/create-new-review", verifyToken, createNewReview);
 router.get("/get-best-selling-products", getBestSellingProducts);
+
+export default router;
