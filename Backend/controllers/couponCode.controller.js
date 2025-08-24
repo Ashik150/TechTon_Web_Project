67a6f29f10e9d74ef8b623b9be15e8ea699f27fc
Ashik150 <sskhan4409@gmail.com>
2025-08-24 08:15:32 +0600

    create coupon code api done

--- Backend/controllers/couponCode.controller.js
+++ Backend/controllers/couponCode.controller.js
@@ -1,3 +1,21 @@
 import { Shop } from '../models/shop.model.js';
 import ErrorHandler from '../Utils/ErrorHandler.js';
 import { CouponCode } from '../models/couponCode.model.js';
+
+
+export const createCouponCode = async (req, res, next) => {
+    try {
+        const isCouponCodeExists = await CouponCode.find({
+            name: req.body.name,
+        });
+
+        const couponCode = await CouponCode.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            couponCode,
+        });
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+};
