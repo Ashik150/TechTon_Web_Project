6aeee40813fa6b9173062d8d07082284d339290a
Ashik150 <sskhan4409@gmail.com>
2025-08-24 08:21:53 +0600

    createEvent validation set

--- Backend/controllers/event.controller.js
+++ Backend/controllers/event.controller.js
@@ -2,3 +2,15 @@ import cloudinary from 'cloudinary';
 import { Shop } from '../models/shop.model.js';
 import ErrorHandler from '../Utils/ErrorHandler.js';
 import { Event } from '../models/event.model.js';
+
+export const createEvent = async (req, res, next) => {
+    try {
+        const shopId = req.body.shopId;
+        const shop = await Shop.findById(shopId);
+        if (!shop) {
+            return next(new ErrorHandler('Shop not found', 400));
+        } 
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+};
