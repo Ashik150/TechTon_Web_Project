12cc6aa7e56f6e966d2b52a7c636e975279c815f
Ashik150 <sskhan4409@gmail.com>
2025-08-24 08:22:30 +0600

    createimage api done

--- Backend/controllers/event.controller.js
+++ Backend/controllers/event.controller.js
@@ -9,7 +9,30 @@ export const createEvent = async (req, res, next) => {
         const shop = await Shop.findById(shopId);
         if (!shop) {
             return next(new ErrorHandler('Shop not found', 400));
-        } 
+        } else {
+            let images = [];
+
+            if (typeof req.body.images === "string") {
+                images.push(req.body.images);
+            } else {
+                images = req.body.images;
+            }
+
+            const imagesLinks = [];
+
+
+            const productData = req.body;
+            productData.images = imagesLinks;
+            productData.shop = shop;
+
+            const event = await Event.create(productData);
+
+            res.status(201).json({
+                success: true,
+                event,
+            });
+        }
+
     } catch (error) {
         return next(new ErrorHandler(error.message, 400));
     }
