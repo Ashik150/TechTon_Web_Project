be85fdc6f4d64458be5c023a135ba0d97045bc1e
Ayonspoiler <ayon4052@gmail.com>
2025-08-26 10:04:41 +0600

    processpayment module created.

--- Backend/controllers/payment.controller.js
+++ Backend/controllers/payment.controller.js
@@ -1,2 +1,17 @@
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+
+export const processpayment = async (req, res, next) => {
+  const myPayment = await stripe.paymentIntents.create({
+    amount: req.body.amount,
+    currency: "USD",
+    metadata: {
+      company: "TechTon",
+    },
+  });
+  res.status(200).json({
+    success: true,
+    client_secret: myPayment.client_secret,
+  });
+};
