065bd4efcaeaeccdf1f49277c9d52341a419029e
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-08-26 21:18:42 +0600

    handle msg and other features updated

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -110,66 +110,127 @@ const ProductDetails = ({ data }) => {
   console.log(averageRating);
 
   return (
-    <div>
-      <div className="w-full 800px:w-[50%]">
-        <img
-          src={`${data && data.images[select]?.url}`}
-          alt=""
-          className="w-[80%]"
-        />
-        <div className="w-full flex">
-          {data &&
-            data.images.map((i, index) => (
-              <div
-                className={`${select === 0 ? "border" : "null"} cursor-pointer`}
-              >
+    <div className="bg-white">
+      {data ? (
+        <div className={`${styles.section} w-[80%]`}>
+          <div className="w-full py-5">
+            <div className="block w-full 800px:flex">
+              <div className="w-full 800px:w-[50%]">
                 <img
-                  src={`${i?.url}`}
+                  src={`${data && data.images[select]?.url}`}
                   alt=""
-                  className="h-[200px] overflow-hidden mr-3 mt-3"
-                  onClick={() => setSelect(index)}
+                  className="w-[80%]"
                 />
+                <div className="w-full flex">
+                  {data &&
+                    data.images.map((i, index) => (
+                      <div
+                        className={`${select === 0 ? "border" : "null"
+                          } cursor-pointer`}
+                      >
+                        <img
+                          src={`${i?.url}`}
+                          alt=""
+                          className="h-[200px] overflow-hidden mr-3 mt-3"
+                          onClick={() => setSelect(index)}
+                        />
+                      </div>
+                    ))}
+                  <div
+                    className={`${select === 1 ? "border" : "null"
+                      } cursor-pointer`}
+                  ></div>
+                </div>
               </div>
-            ))}
-          <div
-            className={`${select === 1 ? "border" : "null"} cursor-pointer`}
-          ></div>
+              <div className="w-full 800px:w-[50%] pt-5">
+                <h1 className={`${styles.productTitle}`}>{data.name}</h1>
+                <p>{data.description}</p>
+                <div className="flex pt-3">
+                  <h4 className={`${styles.productDiscountPrice}}`}>
+                    BDT {data.discountPrice}
+                  </h4>
+                  <h3 className={`${styles.price}`}>
+                    {data.originalPrice ? "BDT " + data.originalPrice : null}
+                  </h3>
+                </div>
+                <div className="flex items-center mt-12 justify-between pr-3">
+                  <div>
+                    <button
+                      className="bg-gradient-to-r from-lime-700 to-lime-800 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
+                      onClick={decrementCount}
+                    >
+                      -
+                    </button>
+                    <span className="bg-gray-200 text-gray-800 font-medium px-4 py-[11px]">
+                      {count}
+                    </span>
+                    <button
+                      className="bg-gradient-to-r from-lime-700 to-lime-800 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
+                      onClick={incrementCount}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div>
+                    {click ? (
+                      <AiFillHeart
+                        size={30}
+                        className="cursor-pointer"
+                        onClick={() => removeFromWishlistHandler(data)}
+                        color={click ? "red" : "#333"}
+                        title="Remove from wishlist"
+                      />
+                    ) : (
+                      <AiOutlineHeart
+                        size={30}
+                        className="cursor-pointer"
+                        onClick={() => addToWishlistHandler(data)}
+                        title="Add to wishlist"
+                      />
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={`${styles.button} mt-6 rounded-[4px] h-11 flex items-center bg-[#355E3B]`}
+                  onClick={() => addToCartHandler(data._id)}
+                >
+                  <span className="text-white flex items-center">
+                    Add to cart <AiOutlineShoppingCart className="ml-1" />
+                  </span>
+                </div>
+                <div className="flex items-center pt-8">
+                  <Link to={`/shop/preview/${data?.shop._id}`}>
+                      <img
+                        src={`${Seller.avatar?.url}`} // Adjust the field name for the seller's avatar URL
+                        alt="Seller Avatar"
+                        className="w-[50px] h-[50px] rounded-full mr-2"
+                      />
+                  </Link>
+                  <div className="pr-8">
+                    <Link to={`/shop/preview/${data?.shop._id}`}>
+                      <h3 className={`${styles.shop_name} pb-1 pt-1`}>
+                        {data.shop.name}
+                      </h3>
+                    </Link>
+                    <h5 className="pb-3 text-[15px]">
+                      ({averageRating}/5) Ratings
+                    </h5>
+                  </div>
+                  <div
+                    className={`${styles.button} bg-[#355E3B] mt-4 rounded h-11`}
+                    onClick={handleMessageSubmit}
+                  >
+                    <span className="text-white flex items-center">
+                      Send Message <AiOutlineMessage className="ml-1" />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <ProductDetailsInfo data={data} products={products} totalReviewsLength={totalReviewsLength} averageRating={averageRating} Seller={Seller} />
         </div>
-        <div className="flex pt-3">
-          <h4 className={`${styles.productDiscountPrice}}`}>
-            BDT {data.discountPrice}
-          </h4>
-          <h3 className={`${styles.price}`}>
-            {data.originalPrice ? "BDT " + data.originalPrice : null}
-          </h3>
-        </div>
-        <div>
-          {click ? (
-            <AiFillHeart
-              size={30}
-              className="cursor-pointer"
-              onClick={() => removeFromWishlistHandler(data)}
-              color={click ? "red" : "#333"}
-              title="Remove from wishlist"
-            />
-          ) : (
-            <AiOutlineHeart
-              size={30}
-              className="cursor-pointer"
-              onClick={() => addToWishlistHandler(data)}
-              title="Add to wishlist"
-            />
-          )}
-        </div>
-        <div
-          className={`${styles.button} mt-6 rounded-[4px] h-11 flex items-center bg-[#355E3B]`}
-          onClick={() => addToCartHandler(data._id)}
-        >
-          <span className="text-white flex items-center">
-            Add to cart <AiOutlineShoppingCart className="ml-1" />
-          </span>
-        </div>
-      </div>
+      ) : null}
     </div>
   );
 };
