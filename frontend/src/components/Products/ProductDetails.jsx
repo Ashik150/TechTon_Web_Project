6c2f463514fb828ba533aa0ae5908adefd60fde6
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-08-26 20:55:10 +0600

    imported necessary hooks and effects

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,9 +1,44 @@
-const ProductDetails = () => {
-    return (
-        <div>
-            <h1>Product Details</h1>
-        </div>
-    );
-}
- 
+import React, { useEffect, useState } from "react";
+const ProductDetails = ({data}) => {
+  const { wishlist } = useSelector((state) => state.wishlist);
+  const { cart } = useSelector((state) => state.cart);
+  const [count, setCount] = useState(1);
+  const [click, setClick] = useState(false);
+  const [Seller, setSeller] = useState("");
+  const [select, setSelect] = useState(1);
+  const navigate = useNavigate();
+  const { products } = useSelector((state) => state.products);
+  const { seller } = useSelector((state) => state.seller);
+  const { user } = useAuthStore();
+  const dispatch = useDispatch();
+
+   useEffect(() => {
+    dispatch(getAllProductsShop(data && data?.shop._id));
+    if (wishlist && wishlist.find((i) => i._id === data?._id)) {
+      setClick(true);
+    } else {
+      setClick(false);
+    }
+
+    if (data && data.shop && seller && seller._id === data.shop._id) {
+      setSeller(seller); // Set the entire seller object
+    }
+  }, [data, wishlist, seller]);
+
+  console.log("Seller: ", Seller.avatar?.url);
+
+  const decrementCount = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+
+  return (
+    <div>
+      <h1>Product Details</h1>
+    </div>
+  );
+};
+
 export default ProductDetails;
