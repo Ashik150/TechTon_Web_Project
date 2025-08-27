ff6dfe84cfdf3afef295015993921e260ff4fdce
Abrar Mubeen <abrarmubeen@iut-dhaka.edu>
2025-08-28 00:15:59 +0600

    Shop create page added

---
+++ frontend/src/pages/Shop/ShopCreatePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ShopCreate from '../components/Shop/ShopCreate'
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+
+const ShopCreatePage = () => {
+  const navigate = useNavigate();
+  const { isSeller, seller } = useSelector((state) => state.seller);
+
+  useEffect(() => {
+    if (isSeller === true) {
+      navigate(`/shop/${seller._id}`);
+    }
+  }, [])
+  return (
+    <div><ShopCreate /></div>
+  )
+}
+
+export default ShopCreatePage
