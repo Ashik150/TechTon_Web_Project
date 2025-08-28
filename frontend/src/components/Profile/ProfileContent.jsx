2bd59c6e345f1ced97baaec034b70516283d859b
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-08-28 10:38:01 +0600

    profile comp hooks

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 const ProfileContent = () => {
-    return (
-        <div>
-            <h1>User Profile</h1>
-            <p>Welcome to your profile page!</p>
-        </div>
-    );
-}
- 
+  const { user } = useAuthStore();
+  const { error, successMessage } = useSelector((state) => state.user);
+  const [name, setName] = useState(user && user.name);
+  const [email, setEmail] = useState(user && user.email);
+  const [phoneNumber, setPhoneNumber] = useState(user && user.phoneNumber);
+  const [password, setPassword] = useState();
+  const [avatar, setAvatar] = useState(null);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Welcome to your profile page!</p>
+    </div>
+  );
+};
+
 export default ProfileContent;
