35cde473ef2548e2de756e9b4e264af7222bbe6a
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-08-28 11:42:55 +0600

    refund orders functionality

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -277,4 +277,89 @@ const AllOrders = () => {
   );
 };
 
+const AllRefundOrders = () => {
+  const { user } = useAuthStore();
+  const { orders } = useSelector((state) => state.order);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllOrdersOfUser(user._id));
+  }, []);
+
+  const eligibleOrders =
+    orders && orders.filter((item) => item.status === "Processing refund");
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 200, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 180,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.row.status === "delivered" ? "greenColor" : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 100,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/order/${params.id}`}>
+              <Button>
+                <AiOutlineArrowRight size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  eligibleOrders &&
+    eligibleOrders.forEach((item) => {
+      row.push({
+        id: item._id,
+        itemsQty: item.cart.length,
+        total: item.totalPrice + " BDT",
+        status: item.status,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSize={10}
+        disableSelectionOnClick
+      />
+    </div>
+  );
+};
+
 export default ProfileContent;
