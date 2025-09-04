a477aea4a833d68d0fa534e0cafcf9aab4593afa
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-09-05 00:02:44 +0600

    cartData

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -414,3 +414,30 @@ const PaymentInfo = ({
     );
 };
 
+const CartData = ({ orderData }) => {
+    const shipping = orderData?.shipping?.toFixed(2);
+    return (
+        <div className="w-full bg-[#fff] rounded-md p-5 pb-8">
+            <div className="flex justify-between">
+                <h3 className="text-[16px] font-[400] text-[#000000a4]">Subtotal:</h3>
+                <h5 className="text-[18px] font-[600]">{orderData?.subTotalPrice} BDT</h5>
+            </div>
+            <br />
+            <div className="flex justify-between">
+                <h3 className="text-[16px] font-[400] text-[#000000a4]">Shipping:</h3>
+                <h5 className="text-[18px] font-[600]">{shipping} BDT</h5>
+            </div>
+            <br />
+            <div className="flex justify-between border-b pb-3">
+                <h3 className="text-[16px] font-[400] text-[#000000a4]">Discount:</h3>
+                <h5 className="text-[18px] font-[600]">{orderData?.discountPrice ? orderData.discountPrice : "-"} BDT</h5>
+            </div>
+            <h5 className="text-[18px] font-[600] text-end pt-3">
+                {orderData?.totalPrice} BDT
+            </h5>
+            <br />
+        </div>
+    );
+};
+
+export default Payment;
