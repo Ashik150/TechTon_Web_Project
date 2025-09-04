71c8d2d6f98fdbb3a3d300771b64b9cf4f88a53f
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-09-05 00:01:46 +0600

    setup

---
+++ frontend/src/components/Payment/Payment.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "../../styles/styles";
+import { useEffect } from "react";
+import {
+    CardNumberElement,
+    CardCvcElement,
+    CardExpiryElement,
+    useStripe,
+    useElements,
+} from "@stripe/react-stripe-js";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+import { RxCross1 } from "react-icons/rx";
+import { useAuthStore } from "../../store/authStore";
+
+
+
+
+
