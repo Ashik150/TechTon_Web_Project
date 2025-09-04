9dd3bf0b692881734416671bc4f06e1b2be905c2
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-09-05 00:01:59 +0600

    payment

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -17,7 +17,190 @@ import { toast } from "react-toastify";
 import { RxCross1 } from "react-icons/rx";
 import { useAuthStore } from "../../store/authStore";
 
+const Payment = () => {
+    const [orderData, setOrderData] = useState([]);
+    const [open, setOpen] = useState(false);
+    const { user } = useAuthStore();
+    const navigate = useNavigate();
+    const stripe = useStripe();
+    const elements = useElements();
 
+    useEffect(() => {
+        const orderData = JSON.parse(localStorage.getItem("latestOrder"));
+        setOrderData(orderData);
+    }, []);
+
+    const createOrder = (data, actions) => {
+        return actions.order
+            .create({
+                purchase_units: [
+                    {
+                        description: "Sunflower",
+                        amount: {
+                            currency_code: "USD",
+                            value: orderData?.totalPrice,
+                        },
+                    },
+                ],
+                // not needed if a shipping address is actually needed
+                application_context: {
+                    shipping_preference: "NO_SHIPPING",
+                },
+            })
+            .then((orderID) => {
+                return orderID;
+            });
+    };
+
+    const order = {
+        cart: orderData?.cart,
+        shippingAddress: orderData?.shippingAddress,
+        user: user && user,
+        totalPrice: orderData?.totalPrice,
+    };
+
+    const onApprove = async (data, actions) => {
+        return actions.order.capture().then(function (details) {
+            const { payer } = details;
+
+            let paymentInfo = payer;
+
+            if (paymentInfo !== undefined) {
+                paypalPaymentHandler(paymentInfo);
+            }
+        });
+    };
+
+    const paypalPaymentHandler = async (paymentInfo) => {
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        order.paymentInfo = {
+            id: paymentInfo.payer_id,
+            status: "succeeded",
+            type: "Paypal",
+        };
+        console.log(order.paymentInfo);
+        await axios
+            .post(`${server}/order/create-order`, order, config)
+            .then((res) => {
+                setOpen(false);
+                navigate("/order/success");
+                toast.success("Order successful!");
+                localStorage.setItem("cartItems", JSON.stringify([]));
+                localStorage.setItem("latestOrder", JSON.stringify([]));
+                setTimeout(() => {
+                    window.location.reload();
+                }, 2000);
+            });
+    };
+
+    const paymentData = {
+        amount: Math.round(orderData?.totalPrice * 100),
+    };
+
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+
+            const { data } = await axios.post(
+                `${server}/payment/process`,
+                paymentData,
+                config
+            );
+            console.log("data: ", data);
+            const client_secret = data.client_secret;
+            //console.log(client_secret);
+
+            if (!stripe || !elements) return;
+            const result = await stripe.confirmCardPayment(client_secret, {
+                payment_method: {
+                    card: elements.getElement(CardNumberElement),
+                },
+            });
+
+            if (result.error) {
+                toast.error(result.error.message);
+            } else {
+                console.log(result);
+                if (result.paymentIntent.status === "succeeded") {
+                    order.paymentInfo = {
+                        id: result.paymentIntent.id,
+                        status: result.paymentIntent.status,
+                        type: "Credit Card",
+                    };
+                    console.log(order.paymentInfo);
+                    await axios
+                        .post(`${server}/order/create-order`, order, config)
+                        .then((res) => {
+                            setOpen(false);
+                            navigate("/order/success");
+                            toast.success("Order successful!");
+                            localStorage.setItem("cartItems", JSON.stringify([]));
+                            localStorage.setItem("latestOrder", JSON.stringify([]));
+                            window.location.reload();
+                        });
+                }
+            }
+        } catch (error) {
+            toast.error(error);
+        }
+    };
+
+    const cashOnDeliveryHandler = async (e) => {
+        e.preventDefault();
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        order.paymentInfo = {
+            type: "Cash On Delivery",
+        };
+
+        await axios
+            .post(`${server}/order/create-order`, order, config)
+            .then((res) => {
+                setOpen(false);
+                navigate("/order/success");
+                toast.success("Order successful!");
+                localStorage.setItem("cartItems", JSON.stringify([]));
+                localStorage.setItem("latestOrder", JSON.stringify([]));
+                window.location.reload();
+            });
+    };
+
+    return (
+        <div className="w-full flex flex-col items-center py-8">
+            <div className="w-[90%] 1000px:w-[70%] block 800px:flex">
+                <div className="w-full 800px:w-[65%]">
+                    <PaymentInfo
+                        user={user}
+                        open={open}
+                        setOpen={setOpen}
+                        onApprove={onApprove}
+                        createOrder={createOrder}
+                        paymentHandler={paymentHandler}
+                        cashOnDeliveryHandler={cashOnDeliveryHandler}
+                    />
+                </div>
+                <div className="w-full 800px:w-[35%] 800px:mt-0 mt-8">
+                    <CartData orderData={orderData} />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 
 
