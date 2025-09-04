ca4645e29a6ab857b073a287752111c6b3608ee3
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-09-04 23:51:53 +0600

    Display the latest event using the EventCard component.

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -13,3 +13,21 @@ const Events = () => {
 
   useEffect(() => {
     dispatch(getAllEvents());
+  }, [dispatch]);
+   
+  return (
+    <div>
+     {
+      !isLoading && (
+        <div className={`${styles.section}`}>
+      <div className={`${styles.heading}`}>
+        <h1>Popular Events</h1>
+      </div>
+
+      <div className="w-full grid">
+         {
+          allEvents.length !== 0 && (
+            <EventCard data={allEvents && allEvents[0]} />
+          )
+         }
+ 
