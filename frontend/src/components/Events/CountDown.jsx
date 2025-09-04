72384e50ee0efabcf97a355b8ac9038c1392f5ad
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-09-04 23:49:27 +0600

    Add navigation link to event-specific product details page.

--- frontend/src/components/Events/CountDown.jsx
+++ frontend/src/components/Events/CountDown.jsx
@@ -37,3 +37,27 @@ const CountDown = ({ data={} }) => {
     return timeLeft;
   }
 
+  const timerComponents = Object.keys(timeLeft).map((interval) => {
+    if (!timeLeft[interval]) {
+      return null;
+    }
+
+    return (
+      <span className="text-[25px] text-[#475ad2]">
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? (
+        timerComponents
+      ) : (
+        <span className="text-[red] text-[25px]">Time's Up</span>
+      )}
+    </div>
+  );
+};
+
+export default CountDown;
