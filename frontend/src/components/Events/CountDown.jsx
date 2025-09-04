766377a7e79b69b488097f652a6dea985ea4a76b
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-09-04 23:48:49 +0600

    Add stock checking and user notifications for cart actions.

--- frontend/src/components/Events/CountDown.jsx
+++ frontend/src/components/Events/CountDown.jsx
@@ -26,3 +26,14 @@ const CountDown = ({ data={} }) => {
     let timeLeft = {};
 
     if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  }
+
