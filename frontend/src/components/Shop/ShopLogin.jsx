a4c26a3b304e895ef6ef3d2ca9580f034e39f3b7
irfanAbir1231 <irfanhakim@iut-dhaka.edu>
2025-09-05 00:08:43 +0600

    login

---
+++ frontend/src/components/Shop/ShopLogin.jsx
@@ -0,0 +1,147 @@
+import { React, useEffect, useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import styles from "../../styles/styles";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+import FloatingShape from "../floatingShape";
+
+
+const ShopLogin = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [visible, setVisible] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await axios
+            .post(
+                `${server}/shop/login-shop`,
+                {
+                    email,
+                    password,
+                },
+                { withCredentials: true }
+            )
+            .then((res) => {
+                toast.success("Login Success!");
+                navigate("/shopdashboard");
+                window.location.reload(true);
+            })
+            .catch((err) => {
+                toast.error('Invalid email or password!');
+            });
+    };
+
+    return (
+        <div className="min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex flex-col items-center justify-center relative overflow-hidden">
+            <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+            <FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+            <FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <h2 className="text-center text-3xl font-extrabold text-gray-800 mb-6">
+                        Sign in to your Shop
+                    </h2>
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Email address
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    autoComplete="email"
+                                    required
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Password
+                            </label>
+                            <div className="mt-1 relative">
+                                <input
+                                    type={visible ? "text" : "password"}
+                                    name="password"
+                                    autoComplete="current-password"
+                                    required
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                />
+                                {visible ? (
+                                    <AiOutlineEye
+                                        className="absolute right-2 top-2 cursor-pointer"
+                                        size={25}
+                                        onClick={() => setVisible(false)}
+                                    />
+                                ) : (
+                                    <AiOutlineEyeInvisible
+                                        className="absolute right-2 top-2 cursor-pointer"
+                                        size={25}
+                                        onClick={() => setVisible(true)}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                        <div className={`${styles.noramlFlex} justify-between`}>
+                            <div className={`${styles.noramlFlex}`}>
+                                <input
+                                    type="checkbox"
+                                    name="remember-me"
+                                    id="remember-me"
+                                    className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                                />
+                                <label
+                                    htmlFor="remember-me"
+                                    className="ml-2 block text-sm text-gray-900"
+                                >
+                                    Remember me
+                                </label>
+                            </div>
+                            <div className="text-sm">
+                                <a
+                                    href=".forgot-password"
+                                    className="font-medium text-green-600 hover:text-green-800"
+                                >
+                                    Forgot your password?
+                                </a>
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                type="submit"
+                                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-700 hover:bg-green-900"
+                            >
+                                Sign In
+                            </button>
+                        </div>
+                        <div className={`${styles.noramlFlex} w-full`}>
+                            <h4>Not have any account?</h4>
+                            <Link to="/shop-create" className="text-green-800 pl-2">
+                                Sign Up
+                            </Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ShopLogin;
