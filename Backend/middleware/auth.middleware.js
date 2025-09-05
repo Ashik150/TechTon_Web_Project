24e8241980bbfdd4addd3ffd372be2cb947b47ef
Ashik150 <sskhan4409@gmail.com>
2025-09-05 10:02:25 +0600

    isAuthenticated logic added in middleware

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,14 @@
+import { User } from "../models/user.model.js";
+import { Shop } from "../models/shop.model.js";
+import { ErrorHandler } from '../Utils/ErrorHandler.js';
+import jwt from "jsonwebtoken";
+
+export const isAuthenticated = async(req,res,next) => {
+    const {token} = req.cookies.token;
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+};
