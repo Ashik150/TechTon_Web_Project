61a6ec3533aa60117c2c4373509334d0c60a5746
Ashik150 <sskhan4409@gmail.com>
2025-09-05 10:04:31 +0600

    token validation added in auth middleware

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -6,6 +6,10 @@ import jwt from "jsonwebtoken";
 export const isAuthenticated = async(req,res,next) => {
     const {token} = req.cookies.token;
 
+     if(!token){
+        return next(new ErrorHandler("Please login to continue", 401));
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     req.user = await User.findById(decoded.id);
